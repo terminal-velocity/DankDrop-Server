a39f5308231bea3453ef3427bdec440d19127352
Tim Stallard <code@timstallard.me.uk>
2016-10-01 19:30:52 +0100

    Removed old test endpoint

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,6 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/api/test", require("./routes/api/test.js"));
-
 router.use("client", express.static("frontend/"));
 
 module.exports = router;
