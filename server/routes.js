1b88d0c8ac92650fbc2b65592c6b53c404fedc20
Tim Stallard <code@timstallard.me.uk>
2016-10-01 19:47:49 +0100

    Added start of dropPoints API

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,9 @@ var router = express.Router();
 // User routes
 router.post("/api/user", require("./routes/api/user/post.js"));
 
+//dropPoints routes
+router.post("/api/dropPoints/by-uuid/:id", require("./routes/api/dropPoints/by-uuid/get.js"));
+
 router.use("client", express.static("frontend/"));
 
 module.exports = router;
