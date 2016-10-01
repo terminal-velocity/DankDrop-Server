75e9bc295c9e1823eae213996ce1c808c04cb0ca
freddieb <freddieiabrown@hotmail.co.uk>
2016-10-01 19:37:52 +0100

    User route post

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,9 @@ var router = express.Router();
 
 router.get("/api/test", require("./routes/api/test.js"));
 
+// User routes
+router.post("/api/user", require("./routes/api/user/post.js"));
+
 router.use("client", express.static("frontend/"));
 
 module.exports = router;
