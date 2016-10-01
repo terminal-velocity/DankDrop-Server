3203d6ff589bddfd01956c20d82403654772479c
Tim Stallard <code@timstallard.me.uk>
2016-10-01 19:11:31 +0100

    Added meme model

---
+++ server/models/Meme.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose')
+
+var voteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  rating: {
+    type: Number
+  }
+});
+
+var memeSchema = new mongoose.Schema({
+  img: {
+    data: {
+      type: Buffer
+    },
+    contentType: {
+      type: String
+    }
+  },
+  name: {
+    type: String
+  },
+  votes: [voteSchema],
+  dropPoint: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "DropPoint"
+  }
+});
+
+module.exports = mongoose.model("Meme", memeSchema);
